28581a62106c645ecaa919f49237d23ba2ee435d
Sujitha Wijewantha <sujithacrysis@gmail.com>
2024-06-03 11:48:41 +0530

    Fix: fix build issue

--- app/components/AboutMe.tsx
+++ app/components/AboutMe.tsx
@@ -1,26 +1,53 @@
 "use client";
 
 import Image from "next/image";
-import { DOWN_ICON } from "../../utils/assets";
+import { DOWN_ICON, WAVE_IMAGE } from "../../utils/assets";
+import { useSpring, animated } from "@react-spring/web";
 
 const AboutMe = () => {
   const scrollToAboutMe = () => {
     document.querySelector("#about-me")?.scrollIntoView();
   };
 
+  const styles = useSpring({
+    from: { rotate: 0 },
+    to: [
+      { rotate: 25, config: { duration: 150 } }, // Rotate to 25 degrees
+      { rotate: 0, config: { duration: 150 } }, // Rotate back to 0 degrees
+    ],
+    config: { duration: 300 }, // Animation duration
+  });
+
   return (
-    <div
-      className="w-full flex justify-center mt-auto mb-10 cursor-pointer"
-      onClick={scrollToAboutMe}
-    >
-      <Image
-        width={30}
-        height={30}
-        className="animate-bounce filter-gray-color "
-        src={DOWN_ICON}
-        alt="Click Me"
-      />
-    </div>
+    <>
+      <div className="flex gap-2 items-center text-xl">
+        Hello there{" "}
+        <animated.div style={{ display: "inline-block", ...styles }}>
+          <Image width="20" height="20" src={WAVE_IMAGE} alt="👋" />{" "}
+        </animated.div>
+        {"I'm"}
+      </div>
+      <div className="text-name-gradient text-6xl font-semibold  ">
+        Sujitha Wijewantha
+      </div>
+      <div className="text-3xl  ">I make stuff for the web</div>
+      <div className="mt-2 text-xl">
+        {"I'm "}a software engineer that specializes in building amazing
+        products.
+      </div>
+      <div
+        className="w-full flex justify-center mt-auto mb-10 cursor-pointer"
+        onClick={scrollToAboutMe}
+      >
+        <Image
+          width={30}
+          height={30}
+          className="animate-bounce filter-gray-color "
+          src={DOWN_ICON}
+          alt="Click Me"
+        />
+      </div>
+    </>
   );
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { Button } from "@/components/ui/Button";
-import Image, { StaticImageData } from "next/image";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "../components/ui/Tabs";
-import { WAVE_IMAGE, ME_IMAGE, INBOX_IMAGE, LINK_ICON } from "../utils/assets";
+import Image from "next/image";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "../components/ui/Tabs";
+import { ME_IMAGE, INBOX_IMAGE, LINK_ICON } from "../utils/assets";
 import AboutMe from "./components/AboutMe";
 import Navbar from "./components/Navbar";
 import { Metadata } from "next";
@@ -18,18 +23,6 @@ const Index = () => {
         <Navbar />
         <div className="container">
           <div className="pt-60 h-screen flex flex-col">
-            <div className="flex gap-2 items-center text-xl">
-              Hello there{" "}
-              <Image width="20" height="20" src={WAVE_IMAGE} alt="👋" /> {"I'm"}
-            </div>
-            <div className="text-name-gradient text-6xl font-semibold  ">
-              Sujitha Wijewantha
-            </div>
-            <div className="text-3xl  ">I make stuff for the web</div>
-            <div className="mt-2 text-xl">
-              {"I'm "}a software engineer that specializes in building amazing
-              products.
-            </div>
             <AboutMe />
           </div>
           <div id="about-me">
@@ -193,59 +186,6 @@ const Index = () => {
         </div>
       </footer>
     </>
-  );
-};
-
-interface WorkCard {
-  stack: string;
-  title: string;
-  image: StaticImageData | string;
-  description: string;
-  position: "LEFT" | "RIGHT";
-  link?: string;
-}
-export const WorkCard: React.FC<WorkCard> = ({
-  description,
-  position,
-  stack,
-  title,
-  link,
-  image,
-}) => {
-  return (
-    <div
-      className={`flex items-center relative flex-col ${
-        position === "RIGHT" ? "md:flex-row-reverse" : "md:flex-row"
-      }`}
-    >
-      <div className="brightness-[0.4] hover:brightness-100 transition-all rounded-md overflow-hidden max-h-64 max-w-md w-40 relative">
-        {link ? (
-          <a href={link} target="_blank" rel="noopener noreferrer">
-            <Image layout="fill" src={image} alt="" />
-          </a>
-        ) : (
-          <Image layout="fill" src={image} alt="" />
-        )}
-      </div>
-      <div
-        className={`md:absolute bg-secondary-black rounded-md px-4 py-5 h-fit md:w-2/4 ${
-          position === "RIGHT" ? "left-0" : "right-0"
-        }`}
-      >
-        <div className="flex justify-between">
-          <div className="font-medium text-xl">{title}</div>
-          <div className="relative">
-            {link && (
-              <a href={link} target="_blank" rel="noopener noreferrer">
-                <Image src={LINK_ICON} width={15} height={15} alt="link" />
-              </a>
-            )}
-          </div>
-        </div>
-        <div className="text-base mt-4">{description}</div>
-        <div className="text-gray-600 mt-2">{stack}</div>
-      </div>
-    </div>
   );
 };
 

3ba051ca68c0df0eaad71a59c43badc207a9d9c7
Sujitha Wijewantha <sujithacrysis@gmail.com>
2024-06-03 11:46:12 +0530

    Fix: fix env build issue

--- env.mjs
+++ env.mjs
@@ -3,8 +3,23 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    DATABASE_URL: z.string().url(),
+    PORT: z.any().optional(),
+    DATABASE_URL: z
+      .string()
+      .url()
+      .refine(
+        (str) => !str.includes("YOUR_MYSQL_URL_HERE"),
+        "You forgot to change the default URL"
+      ),
+    NODE_ENV: z
+      .enum(["development", "test", "production"])
+      .default("development"),
   },
   client: {},
-  runtimeEnv: process.env,
+  runtimeEnv: {
+    PORT: process.env.PORT,
+    DATABASE_URL: process.env.DATABASE_URL,
+    NODE_ENV: process.env.NODE_ENV,
+  },
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
 });

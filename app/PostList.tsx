0bddcd755d1974bec384040218421a3562eac120
Sujitha Wijewantha <sujithacrysis@gmail.com>
2024-07-16 19:31:40 +0530

    Fix: slugs are not taking properly

--- app/PostList.tsx
+++ app/PostList.tsx
@@ -3,12 +3,13 @@ import React from "react";
 import { format } from "date-fns";
 import Link from "next/link";
 const PostList = async () => {
-  const posts = await listPosts();
+  const posts = await listPosts(void 0);
+
   return (
     <div className="mt-8 gap-2 flex flex-col">
       {posts.data?.map((post) => (
         <div className="flex" key={post.id}>
-          <Link href={"posts/" + post.slug}>
+          <Link href={`posts/${post.slug}`}>
             <div>
               <div className="text-xl">{post.title}</div>
               <div className="text-sm text-gray-200">
